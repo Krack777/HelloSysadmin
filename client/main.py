cb0fdf6b23f13e28127c72db448b47c4bd2d97fa
Krack <131531725+Krack777@users.noreply.github.com>
2024-04-09 23:23:48 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,5 +1,7 @@
 from googleSheets import *
+import time
 import os
+delay = 3
 # config start
 account_file = 'client.json'
 spreadsheet_id = '1y8GEKuaH2vYmerQm_sapIwf1Sf7BnpKSTkF4z5cuEkE' # id гугл таблицы, можно найти в ссылке на таблицу после /d и до /edit
@@ -31,3 +33,9 @@ while True:
 		table.write('')
 		os.system('mpg123 sounds/' + command[1])
 
+	
+	elif 'update=True' in command:
+		table.write('')
+		os.system('python3 updater_client.py')
+		
+	time.sleep(delay)
