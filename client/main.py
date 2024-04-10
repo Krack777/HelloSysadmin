da67e40fd2e8b43fb4ae4ac8524afa0e3b497c74
Krack <131531725+Krack777@users.noreply.github.com>
2024-04-10 21:17:03 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -4,18 +4,10 @@ import subprocess
 import time
 import os
 # config start
-account_file = 'client.json'  # расположение json файла с сервис учёткой(см п.4 readme "#см п.3 раздела "Установка бота и клиента(удалённого пк/доски)")
-spreadsheet_id = '1y8GEKuaH2vYmerQm_sapIwf1Sf7BnpKSTkF4z5cuEkE' # id таблицы(см п.3 readme "#см п.3 раздела "Установка бота и клиента(удалённого пк/доски)")
+account_file = ''  # расположение json файла с сервис учёткой(см п.4 readme "#см п.3 раздела "Установка бота и клиента(удалённого пк/доски)")
+spreadsheet_id = '' # id таблицы(см п.3 readme "#см п.3 раздела "Установка бота и клиента(удалённого пк/доски)")
 delay = '3' #Задержка парсинга таблицы на команды(см п.1 Раздела "Пинкод" и "Задержка клиента")
 # config end
-
-processes = subprocess.getoutput("pgrep -f updater_client.py").split()
-for pid in processes:
-    try:
-        os.kill(int(pid), signal.SIGTERM)
-    except ProcessLookupError:
-        pass
-
 table = Table(account_file=account_file, spreadsheet_id=spreadsheet_id)
 while True:
 	command = table.read()
@@ -43,6 +35,12 @@ while True:
 		table.write('')
 		os.system('mpg123 sounds/' + command[1])
 
+	elif "close_OpenBoard" in command:
+		table.write('')
+		processes = subprocess.getoutput("pgrep -f OpenBoard").split()
+		for pid in processes:
+			try:os.kill(int(pid), signal.SIGTERM)
+			except ProcessLookupError: pass
 	
 	elif 'update=True' in command:
 		table.write('')
